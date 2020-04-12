abbfc6c3404e3bbd404e87787a2dc1a0df3a7332
bhkpriyanka <bhkp1729@gmail.com>
2020-04-12 12:03:44 +0530

    Add files via upload

--- Week9/PythonFiles/all_imports.py
+++ Week9/PythonFiles/all_imports.py
@@ -1,6 +1,7 @@
 import torch
 import torchvision
 import torchvision.transforms as transforms
+import torchvision.datasets as datasets
 
 import matplotlib.pyplot as plt
 import numpy as np
@@ -15,3 +16,13 @@ import torch.optim as optim
 
 from torch.optim.lr_scheduler import MultiStepLR
 from torch.optim.lr_scheduler import ReduceLROnPlateau
+
+import albumentations
+from albumentations import (
+    HorizontalFlip, IAAPerspective, Rotate, CLAHE, RandomRotate90, Crop, Normalize,
+    Transpose, ShiftScaleRotate, Blur, OpticalDistortion, GridDistortion, HueSaturationValue,
+    IAAAdditiveGaussianNoise, GaussNoise, MotionBlur, MedianBlur, IAAPiecewiseAffine,
+    IAASharpen, IAAEmboss, RandomContrast, RandomBrightness, Flip, OneOf, Compose,
+    ElasticTransform,ChannelShuffle, Cutout, CoarseDropout
+)
+from albumentations.pytorch import ToTensor
