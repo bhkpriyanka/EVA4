f2096c4bb050d8e36294157e996e83a254103138
bhkpriyanka <bhkp1729@gmail.com>
2020-04-12 11:48:36 +0530

    Add files via upload

---
+++ Week9/PythonFiles/Transforms/albumentation_tfmn_dataloader.py
@@ -0,0 +1,59 @@
+from all_imports import *
+
+class albumentation_train:
+    def __init__(self):
+        self.albumentation_transform = Compose([HorizontalFlip(),
+                                                Rotate(limit=2),
+                                                HueSaturationValue(hue_shift_limit=3, sat_shift_limit=2),
+                                                # RGBShift(r_shift_limit=50, g_shift_limit=50, b_shift_limit=50, p=0.5),
+                                                Blur(blur_limit = 3,p=0.5),
+                                                Cutout(num_holes=4,max_h_size =4, max_w_size =4),
+                                                Normalize(
+                                                    mean=[0.5, 0.5, 0.5],
+                                                    std=[0.5, 0.5, 0.5],
+                                                ),
+                                                ToTensor()
+                                                ])
+
+    def __call__(self, img):
+        img = np.array(img)
+        img = self.albumentation_transform(image=img)
+        return img['image']
+
+
+class albumentation_test:
+    def __init__(self):
+        self.albumentation_transform = Compose([
+            Normalize(
+               mean=[0.5, 0.5, 0.5],
+                std=[0.5, 0.5, 0.5],
+            ),
+            ToTensor()
+        ])
+
+    def __call__(self, img):
+        img = np.array(img)
+        img = self.albumentation_transform(image=img)
+        return img['image']
+
+
+def train_test_loaders(b_size, n_workers=2):
+    # Download Train/test data----------------------------------------------------------------
+    trainset = torchvision.datasets.CIFAR10(root='./data', train=True,
+                                            download=True, transform=albumentation_train())
+
+    testset = torchvision.datasets.CIFAR10(root='./data', train=False,
+                                           download=True, transform=albumentation_test())
+
+    print('Train Images count', len(trainset))
+    print('Test Images count', len(testset))
+
+    # Define data loaders---------------------------------------------------------------------
+    # Train data loader
+    trainloader = torch.utils.data.DataLoader(trainset, batch_size=b_size,
+                                              shuffle=True, num_workers=n_workers)
+    # Test data loader
+    testloader = torch.utils.data.DataLoader(testset, batch_size=b_size,
+                                             shuffle=False, num_workers=n_workers)
+
+    return trainloader, testloader
